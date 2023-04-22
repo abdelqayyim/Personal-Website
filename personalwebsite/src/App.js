f268fdb3e1c077de569679bfeacdf8b35ab3d228
abdelqayyim <abdelymaster@gmail.com>
2023-04-22 19:36:19 -0400

    First commit of react version

---
+++ personalwebsite/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Fragment } from 'react';
+import Introduction from './components/Introduction';
+
+function App() {
+  return (
+    <div className="main-div">
+      <Introduction></Introduction>
+    </div>
+  );
+}
+
+export default App;
---
+++ personalwebsite/src/components/Introduction.js
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import './Introduction.css';
+
+const Introduction = (props)=>{
+    return (
+        <div className='introduction-parent'>
+            <h3>Hi, my name is</h3>
+            <h1>Abdel Qayyim Yahaya.</h1>
+            <h2>I enjoy learning new Skills.</h2>
+            <p>dfjkahsdklfhklasdjhflkadhsfkljhasdklfhalsdkfhadklshfkajlsdhfkldsahfklhdslkfjh
+                asjdfhlsadkhfklajsdhfkjlashdfjklasdklfhlsadkjfhklsadhfldjksfa-flip-horizontalsdjkfhlkasdhflk
+                lorem3
+            </p>
+        </div>
+    )
+};
+
+export default Introduction;
